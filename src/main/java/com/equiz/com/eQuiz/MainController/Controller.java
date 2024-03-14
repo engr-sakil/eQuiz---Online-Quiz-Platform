e24d04292b8db9c4279a404d7a05619332762ef5
engr-sakil <mohammadsakil2037@gmail.com>
2024-03-14 02:19:59 +0100

    create main controller and index page

--- src/main/java/com/equiz/com/eQuiz/MainController/Controller.java
+++ src/main/java/com/equiz/com/eQuiz/MainController/Controller.java
@@ -1,4 +1,10 @@
 package com.equiz.com.eQuiz.MainController;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class Controller {
+    @RequestMapping("/")
+    public String homepage(){
+        return "index.html";
+    }
 }
