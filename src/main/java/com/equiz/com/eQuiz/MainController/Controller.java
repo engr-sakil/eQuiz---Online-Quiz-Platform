d641b5eef85e75c83c1dc3622e19bff75caae297
engr-sakil <mohammadsakil2037@gmail.com>
2024-03-22 02:44:32 +0100

    design home page

--- src/main/java/com/equiz/com/eQuiz/MainController/Controller.java
+++ src/main/java/com/equiz/com/eQuiz/MainController/Controller.java
@@ -1,9 +1,11 @@
 package com.equiz.com.eQuiz.MainController;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@org.springframework.stereotype.Controller
 public class Controller {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String homepage(){
         return "index.html";
     }
